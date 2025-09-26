8386bd20746e91c63dd4727bc7f8d966854e7ed5
zpeng <zpeng0614@gmail.com>
2025-09-26 19:55:28 +0800

    确定代码结构

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-print("初始化")
-print("alkglge,这个是master分支合并other分支之后的样子")
+import pymupdf as pdf
+from openpyxl import workbook
+import os
 
+current_path = os.getcwd() #获取当前脚本所在目录
+os.chdir("发票")#进入发票目录，以提取相关信息
+
+
+
+
+
+
+
+
+
+def rename_pdf():
+    pass
+
+
+def merge_pdf():
+    pass
+
+
+def excel_pdf():
+    pass
